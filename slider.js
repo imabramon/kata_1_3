5b43f940b56cf1b8f6a6d3c42d8634ce23f7b907
im_abramon <shelopugin.vlad@yandex.ru>
2023-12-04 10:42:53 +0300

    Починил слайдер

--- slider.js
+++ slider.js
@@ -4,7 +4,21 @@ const swiper = new Swiper('.slider',{
         el: '.slider__scroll',
         clickable: true,
     },
+    init: false,
     slideClass: "deviece-card",
     slidesPerView: 'auto',
     spaceBetween: '16px',
+});
+
+if(window.innerWidth < 768){
+    swiper.init();
+}
+
+window.addEventListener('resize', function(){
+    if(window.innerWidth < 768){
+        swiper.init();
+        return;
+    }
+
+    swiper.destroy();
 });

1c553be0bd8e0c4a728d658f120960c890e397ff
im_abramon <shelopugin.vlad@yandex.ru>
2023-12-05 10:35:19 +0300

    вот теперь точно резайз норм

--- slider.js
+++ slider.js
@@ -15,20 +15,29 @@ let swiper = new Swiper(swipperClass,swiperSettings);
 
 if(window.innerWidth < 768){
     swiper.init();
+}else{
+    swiper = undefined;
 }
 
 window.addEventListener('resize', function(){
-    if(window.innerWidth < 768){
-       if(swiper.destroyed !== true){
-        return;
-       }
 
+    if(window.innerWidth < 768){
+       if(swiper === undefined){
         swiper = new Swiper(swipperClass, swiperSettings);
         swiper.init();
         return;
+       }
+        
+        return;
     }
 
-    if(swiper.destroyed !== true){
-        swiper.destroy(true, true);
+    if(swiper !== undefined){
+        swiper =  destroySwipper(swiper);
     }
 });
+
+function destroySwipper(swiper){
+    swiper.destroy(true, true);
+    swiper = undefined;
+    return swiper;
+}

07509bc45e5ce3259181e37a9bf66f9af93c103b
im_abramon <shelopugin.vlad@yandex.ru>
2023-12-16 19:44:20 +0300

    таблица карточки

--- src/js/index.js
+++ src/js/index.js
@@ -5,5 +5,6 @@ import './drop-feedback-menu.js'
 import './slider.js'
 import './brand-slider.js'
 import './service-slider.js'
+import './price-slider.js'
 
 console.log('Works!')
---
+++ src/js/price-slider.js
@@ -0,0 +1,7 @@
+import { makeSlider } from './slider'
+
+makeSlider({
+  swipperClass: 'price-slider',
+  sliderPagination: 'price-slider__pagination',
+  sliderCard: 'price-slider__card'
+})

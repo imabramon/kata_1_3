5d57d78a14dd7cd83bdf46c1bb4bf0265d37de83
im_abramon <shelopugin.vlad@yandex.ru>
2023-12-19 15:45:05 +0300

    выход из меню по клику на блюр

--- src/js/drop-menu.js
+++ src/js/drop-menu.js
@@ -4,10 +4,29 @@ export function initDropMenu(object) {
   let menu = document.querySelector(`.${object.menuClass}`)
   let controls = document.querySelectorAll(`.${object.controlClass}`)
 
+  function toggleState(evt) {
+    console.log('toggleState')
+    elementToBlur.classList.toggle(object.blurClass)
+    menu.classList.toggle(menuClosedClass)
+  }
+
+  function blurCloseFunction(evt) {
+    if (evt.skipThisClick) {
+      return
+    }
+
+    if (evt.currentTarget.classList.contains(object.blurClass)) {
+      toggleState()
+    }
+  }
+
+  elementToBlur.addEventListener('click', blurCloseFunction)
+
   controls.forEach(function (el) {
     el.addEventListener('click', function (evt) {
-      elementToBlur.classList.toggle(object.blurClass)
-      menu.classList.toggle(menuClosedClass)
+      console.log('control click')
+      toggleState()
+      evt.skipThisClick = true
     })
   })
 }
--- todo.md
+++ todo.md
@@ -85,7 +85,7 @@
 
 1. [Макет](https://www.figma.com/file/ttlPwrOE9PxIpxl3FqkIT1/%D0%92%D0%B5%D1%80%D1%81%D1%82%D0%BA%D0%B0-%D0%BF%D1%80%D0%B8%D0%B5%D0%BC%D0%BB%D0%B5%D0%BC%D0%BE%D0%B3%D0%BE-%D0%BC%D0%B0%D0%BA%D0%B5%D1%82%D0%B0?node-id=0%3A312&t=e5yhwOwhdBAmAWN0-1)
 2. [Шрифт](https://webfonts.pro/base-web-fonts/sans-serif-grotesque/897-tt-lakes.html) (начертания TT Lakes Regular, TT Lakes Medium, TT Lakes Bold)
-3. Цвета - см. в макете
+3. _ОК_ Цвета - см. в макете
 4. Контентные тексты - можете использовать тексты из макета и/или "lorem ipsum"
 5. Иконки и изображения - экспортируйте из фигмы
 
